04a7099ad9876e741ad925255dc89994f8865e32
Vishnuvardhan27 <52859701+Vishnuvardhan27@users.noreply.github.com>
2020-08-11 15:53:22 +0530

    Add files via upload

--- Lab3.java
+++ Lab3.java
@@ -30,12 +30,14 @@ class Emp
 	}
 }
 
-public class Lab3 {
+public class Lab3{
 
 	public static void main(String[] args) {
-		Emp[] e = new Emp[5];
-		System.out.println("Please Enter 5 Employee Details :");
+		
 		Scanner sc = new Scanner(System.in);
+		/*System.out.println("Please Enter 5 Employee Details :");
+		Scanner sc = new Scanner(System.in);
+		
 		for(int i=0;i<5;i++)
 		{
 			e[i] = new Emp();
@@ -43,11 +45,61 @@ public class Lab3 {
 			e[i].setEname(sc.next());
 			e[i].setSalary(sc.nextDouble());
 		}
+		
+		
+		
+		
+		//jvisualvm Test
+		System.out.println("Please Enter A Number to Start Memory Allocation and test jvisualvm");
+		sc.nextInt();
+		for(int i=1;i<count;i++)
+		{
+			e[i-1] = new Emp();
+			e[i-1].setEmpno(i);
+			e[i-1].setEname("Nameof " +i);
+			e[i-1].setSalary(i*1000);
+		}
+		System.out.println("Memory Allocation is Done");
+		//System.out.println("Employee Details You have Entered are :");
+		
+		//System.out.println(Arrays.toString(e));*/
+		
+		
+		
+		
+		//Check Garbage Collector Working
+		System.out.println("1st Call");
+		call();
+		System.out.println("2st Call");
+		call();
+		System.out.println("Call to GC");
+		System.gc();
+		System.out.println("3st Call");
+		call();
+		System.out.println("In Main Class");
+		System.out.println("Enter a Number to stop the Execution ");
+		sc.nextInt();
 		sc.close();
-		System.out.println("Employee Details You have Entered are :");
 		
-			System.out.println(Arrays.toString(e));
-		
+	}
+	
+	// Check Garbage Collector Code
+	 public static void call()
+	{
+		 int count=999999;
+		Emp[] e = new Emp[count];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please Enter A Number to Start Memory Allocation and test jvisualvm");
+		sc.nextInt();
+		for(int i=1;i<count;i++)
+		{
+			e[i-1] = new Emp();
+			e[i-1].setEmpno(i);
+			e[i-1].setEname("Nameof " +i);
+			e[i-1].setSalary(i*1000);
+		}
+		System.out.println("Memory Allocation is Done");
+		sc.close();
 	}
 
 }
